01ed88b52f37d87d3099af78fb3abde05f2f1ff8
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2020-11-14 19:45:01 +0100

    added new_kata.bat for easier preparing of new kata files

---
+++ new_kata.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Will take name of new kata as argument and prepare the file by copying template
+
+REM read the name of the file
+set filename=%1
+
+REM copy and delete pytestmark line
+findstr /v /c:"pytestmark" template.py >%filename%.py
