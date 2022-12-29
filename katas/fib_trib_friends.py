29b22c81a383e73b81cc900ea782d879a17e8f00
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2022-12-29 15:18:09 +0100

    solution for fib_trib_friends

---
+++ katas/fib_trib_friends.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+
+def compute(a, b):
+    # implement solution here
+    return sum((a, b))
+
+
+# put arguments and expected results here
+ARGS_RESULTS = [
+    ((1, 25), 26),
+    ((42, 250), 292),
+    ]
+
+
+@pytest.mark.parametrize(
+    ('input_args', 'expected'),
+    ARGS_RESULTS,
+    )
+def test(input_args, expected) -> None:
+    assert compute(*input_args) == expected
+
+
+def main() -> int:
+    for args, result in ARGS_RESULTS:
+        print(compute(*args))
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
